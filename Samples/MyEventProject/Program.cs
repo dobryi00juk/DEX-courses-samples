dca011096eeb53998daf99c780ff6d0a2bb99940
GoodWin <dobryi00juk1@gmail.com>
2019-11-20 18:06:53 +0200

    add events

---
+++ Samples/MyEventProject/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata;
+using Console = System.Console;
+
+namespace MyEventProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = 10;
+            var queue = new Queue<int>();
+            queue.TrimExcess();
+
+            Handler += Display;
+
+            for (int i = 0; i < 15; i++)
+            {
+                if (queue.Count >= size)
+                {
+                    Handler?.Invoke("Queue is full");
+                    break;
+                }
+                else
+                {
+                    queue.Enqueue(i);
+                }
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                if (Equals(queue.Count,0))
+                {
+                    Handler?.Invoke("Queue is empty!");
+                    break;
+                }
+                else
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            Handler -= Display;
+
+            var digit = new Digit(30);
+            digit.handler += Display;
+
+            for (int i = 0; i < 100; i += 10)
+            {
+                digit.Add(i);
+            }
+
+            digit.Add(5000);
+            digit.handler -= Display;
+
+            var prop = new PropChange();
+
+            void PropertyHandler(object s, PropertyChangedEventArgs e)
+            {
+                Console.WriteLine($"Property {e.PropertyName} is changed!");
+            }
+
+            prop.PropertyChanged += PropertyHandler;
+            prop.Prop1 = 111;
+            prop.Prop1 = 222;
+            prop.Prop2 = "123";
+            prop.PropertyChanged -= PropertyHandler;
+        }
+
+        public delegate void Counter(string message);
+        public static event Counter Handler;
+
+        private static void Display(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class Digit
+    {
+        private List<int> List { get; set; }
+        public int Percent { get; set; }
+
+        public delegate void DigitHandler(string message);
+
+        public event DigitHandler handler;
+        public Digit(int percent)
+        {
+            Percent = percent;
+            List = new List<int>();
+        }
+
+        public void Add(int item)
+        {
+            if (List.Count == 0)
+            {
+                List.Add(item);
+                return;
+            }
+
+            if (item / 100 * Percent > List.Average()) 
+            {
+                handler?.Invoke("More than 30 %");
+                return;
+            }
+            else
+            {
+                List.Add(item);
+            }
+        }
+
+    }
+}
---
+++ Samples/MyEventProject/PropChange.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MyEventProject
+{
+    class PropChange : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int _prop1;
+        private string _prop2;
+
+        public int Prop1
+        {
+            get => _prop1;
+            set
+            {
+                _prop1 = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Prop2
+        {
+            get => _prop2;
+            set
+            {
+                _prop2 = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Prop2"));
+                //NotifyPropertyChanged();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
