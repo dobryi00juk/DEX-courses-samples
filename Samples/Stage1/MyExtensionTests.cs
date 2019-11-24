b245979d58d7958f92d930d4b8546b37aa6ec620
GoodWin <dobryi00juk1@gmail.com>
2019-11-24 13:27:12 +0200

    add extentions test

---
+++ Samples/Stage1/MyExtensionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Stage1
+{
+    [TestFixture]
+    public class MyExtensionTests
+    {
+        [Test]
+        public void StringExtensionsTests()
+        {
+            var date = new DateTime(1991, 9, 15);
+            var interval = date - 5.IntExtention();
+            Console.WriteLine(interval);
+        }
+    }
+
+    public static class IntExtensions
+    {
+        public static TimeSpan IntExtention(this int number)
+        {
+            return TimeSpan.FromDays(number);
+        }
+    }
+}
